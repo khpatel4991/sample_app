fd4c8651ae0ff6141d44081125fbcffad9fa0258
KASHYAP PATEL <khpatel4991@gmail.com>
2015-02-23 03:41:25 -0500

    Added Contact Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
   get 'static_pages/help'
@@ -12,7 +10,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
