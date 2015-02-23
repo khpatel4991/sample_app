a28615451bec28a4b8759bfa9d926e938c552226
KASHYAP PATEL <khpatel4991@gmail.com>
2015-02-23 03:06:58 -0500

    Eliminate repetition (solved exercise ~)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
 
